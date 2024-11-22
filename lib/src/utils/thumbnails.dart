36923e93a946b07d1992e390fd993799846752c3
jerrypaulsam <62291043+jerrypaulsam@users.noreply.github.com>
2024-11-22 14:33:17 +0530

    Update thumbnails.dart

--- lib/src/utils/thumbnails.dart
+++ lib/src/utils/thumbnails.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:video_editor/src/controller.dart';
 import 'package:video_editor/src/models/cover_data.dart';
-import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:get_thumbnail_video/video_thumbnail.dart';
 
 Stream<List<Uint8List>> generateTrimThumbnails(
   VideoEditorController controller, {
